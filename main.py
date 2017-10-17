4c39674438b6290038ba562ea812e472189fe975
Todd <chihsuanwu@outlook.com>
2017-10-17 10:25:11 +0800

    add draw_station and drwa_rail method

--- main.py
+++ main.py
@@ -27,55 +27,65 @@ class App(tk.Tk):
         stations = 0
         line = [1, 1]
         for data in self.data['data']:
-            data_type = data['type']
-            if data_type == 'rail':
+            if data['type'] == 'rail':
                 line = data['line']
-                continue
-
-            stations += 1
-            x_pos = 200 * stations - 100
-            y_pos = 150
-            # Draw station name.
-            canvas.create_text(x_pos, 50, text=data['name'])
-
-            main_line = sum(line)
-            for x in range(main_line):
-                dy_pos = y_pos - (main_line - 1) * 8 + x * 16
-                if data_type == 'terminal':
-                    if stations == 1:
-                        canvas.create_line(x_pos - 50, dy_pos, x_pos + 100, dy_pos)
-                    else:
-                        canvas.create_line(x_pos - 100, dy_pos, x_pos + 50, dy_pos)
-                else:
-                    canvas.create_line(x_pos - 100, dy_pos, x_pos + 100, dy_pos)
-
-            if data_type == 'terminal':
-                stop = data['stop']
-                for x in range(stop):
-                    dy_pos = y_pos - (main_line - 1) * 8 + x * 16 - (stop - 2) // 2 * 16
-                    canvas.create_line(x_pos - 50, dy_pos, x_pos + 50, dy_pos, fill='blue')
-                    if stations == 1:
-                        canvas.create_line(x_pos - 50, dy_pos - 4, x_pos - 50, dy_pos + 4)
-                    else:
-                        canvas.create_line(x_pos + 50, dy_pos - 4, x_pos + 50, dy_pos + 4)
+                self.__draw_rail(canvas, data, stations)
             else:
-                stop = data['stop']
-                pass_line = data['pass']
-                for x in range(pass_line[0]):
-                    dy_pos = y_pos - (main_line - 1) * 8 + (line[1] + x) * 16
-                    canvas.create_line(x_pos - 50, dy_pos, x_pos + 50, dy_pos, fill='red')
-                for x in range(pass_line[1]):
-                    dy_pos = y_pos - (main_line - 1) * 8 + (line[1] - 1 - x) * 16
-                    canvas.create_line(x_pos - 50, dy_pos, x_pos + 50, dy_pos, fill='red')
-
-                for x in range(stop[0]):
-                    dy_pos = y_pos - (main_line - 1) * 8 + (line[1] + pass_line[0] + x) * 16
-                    canvas.create_line(x_pos - 50, dy_pos, x_pos + 50, dy_pos, fill='blue')
-                for x in range(stop[1]):
-                    dy_pos = y_pos - (main_line - 1) * 8 + (line[1] - 1 - pass_line[1] - x) * 16
-                    canvas.create_line(x_pos - 50, dy_pos, x_pos + 50, dy_pos, fill='blue')
+                stations += 1
+                self.__draw_station(canvas, data, line, stations)
 
 
+
+
+    def __draw_station(self, canvas, data, line, index):
+        x_pos = 200 * index - 100
+        y_pos = 150
+        # Draw station name.
+        canvas.create_text(x_pos, 40, text=data['name'])
+        if data['type'] == 'terminal':
+            canvas.create_text(x_pos, 55, text='terminal')
+        else:
+            canvas.create_text(x_pos, 55, text='stop time: ' + str(data['stop_time']))
+            canvas.create_text(x_pos, 70, text='pass time: ' + str(data['pass_time']))
+
+        main_line = sum(line)
+
+        if data['type'] == 'terminal':
+            stop = data['stop']
+            for x in range(stop):
+                dy_pos = y_pos - (main_line - 1) * 8 + x * 16 - (stop - 2) // 2 * 16
+                canvas.create_line(x_pos - 50, dy_pos, x_pos + 50, dy_pos, fill='blue')
+                if index == 1:
+                    canvas.create_line(x_pos - 50, dy_pos - 4, x_pos - 50, dy_pos + 4)
+                else:
+                    canvas.create_line(x_pos + 50, dy_pos - 4, x_pos + 50, dy_pos + 4)
+        else:
+            stop = data['stop']
+            pass_line = data['pass']
+            for x in range(pass_line[0]):
+                dy_pos = y_pos - (main_line - 1) * 8 + (line[1] + x) * 16
+                canvas.create_line(x_pos - 50, dy_pos, x_pos + 50, dy_pos, fill='red')
+            for x in range(pass_line[1]):
+                dy_pos = y_pos - (main_line - 1) * 8 + (line[1] - 1 - x) * 16
+                canvas.create_line(x_pos - 50, dy_pos, x_pos + 50, dy_pos, fill='red')
+
+            for x in range(stop[0]):
+                dy_pos = y_pos - (main_line - 1) * 8 + (line[1] + pass_line[0] + x) * 16
+                canvas.create_line(x_pos - 50, dy_pos, x_pos + 50, dy_pos, fill='blue')
+            for x in range(stop[1]):
+                dy_pos = y_pos - (main_line - 1) * 8 + (line[1] - 1 - pass_line[1] - x) * 16
+                canvas.create_line(x_pos - 50, dy_pos, x_pos + 50, dy_pos, fill='blue')
+
+
+    def __draw_rail(self, canvas, data, index):
+        x_pos = 200 * index
+        y_pos = 150
+        # Draw station name.
+        canvas.create_text(x_pos, 70, text='time: ' + str(data['time']))
+        main_line = sum(data['line'])
+        for x in range(main_line):
+            dy_pos = y_pos - (main_line - 1) * 8 + x * 16
+            canvas.create_line(x_pos - 50, dy_pos, x_pos + 50, dy_pos)
 
 def main():
     app = App()
